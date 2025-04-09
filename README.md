1cecd74df52349a8ab586755e73efd85473a2dcf
sherifmag144 <sherifmag144@gmail.com>
2025-04-10 00:22:24 +0200

    test: commit two

---
+++ README.md
@@ -0,0 +1,14 @@
+# Test Automation Project with Cypress
+
+This project contains automated test scripts for Cypress . It also generates reports using Mochawesome.
+
+## Requirements
+
+- **Node.js** (version 10.8.3 or higher)
+- **npm** (version 10.8.3 or higher)
+- **Cypress** (version 14.3.0)
+- **eslint** (version 9.24.0)
+- **eslint-plugin-cypress** (version 4.2.1)
+- **prettier** (version 3.5.3)
+- **Mochawesome** for test reports
+**Clone the repository:** https://github.com/sherifmag144/AmazonTesting.git
--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 1,
+    openMode: 1,
+  },
+  video: false,
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: true,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
