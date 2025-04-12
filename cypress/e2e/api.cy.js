012724fa8bcacaeb344e75ecdb7345a2f1c014f9
sherifmag144 <sherifmag144@gmail.com>
2025-04-12 18:13:51 +0200

    test: edit package

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,33 +1,32 @@
 import _ from 'lodash';
 import body from './RequestBody.json';
 
+it('Api testing', () => {
+  let CreateUserBody = body.CreateUser;
+  let url = 'https://reqres.in/api/users';
+  cy.POSTrequest(url, CreateUserBody).then((response) => {
+    expect(response.status).to.eq(201);
+    expect(response.body.name).to.eq(CreateUserBody.name);
+    expect(response.body.job).to.eq(CreateUserBody.job);
+    expect(response.body.age).to.eq(CreateUserBody.age);
 
-it.only('Api testing', () => {
-    let CreateUserBody = body.CreateUser;
-    let url = 'https://reqres.in/api/users';
-    cy.POSTrequest(url, CreateUserBody).then((response) => {
-      expect(response.status).to.eq(201);
-      expect(response.body.name).to.eq(CreateUserBody.name);
-      expect(response.body.job).to.eq(CreateUserBody.job);
-      expect(response.body.age).to.eq(CreateUserBody.age);
+    let iDresponse = response.body.id;
+    let url2 = `https://reqres.in/api/users/${iDresponse}`;
 
-      let iDresponse = response.body.id;
-      let url2 = `https://reqres.in/api/users/${iDresponse}`;
+    cy.GETrequest(url2).then((response2) => {
+      expect(response2.status).to.eq(404); //Should be 200 but Failed
 
-      cy.GETrequest(url2).then((response2) => {
-        expect(response2.status).to.eq(404); //Should be 201 but Failed
+      let bodyUpdate = _.cloneDeep(CreateUserBody);
+      bodyUpdate.name = 'ahmed';
+      bodyUpdate.job = 'DEV';
+      bodyUpdate.age = '27';
 
-        let bodyUpdate = _.cloneDeep(CreateUserBody);
-        bodyUpdate.name = 'ahmed';
-        bodyUpdate.job = 'DEV';
-        bodyUpdate.age = '27';
-
-        cy.PUTrequest(url2, bodyUpdate).then((response3) => {
-          expect(response3.status).to.eq(200);
-          expect(response3.body.name).to.eq(bodyUpdate.name);
-          expect(response3.body.job).to.eq(bodyUpdate.job);
-          expect(response3.body.age).to.eq(bodyUpdate.age);
-        });
+      cy.PUTrequest(url2, bodyUpdate).then((response3) => {
+        expect(response3.status).to.eq(200);
+        expect(response3.body.name).to.eq(bodyUpdate.name);
+        expect(response3.body.job).to.eq(bodyUpdate.job);
+        expect(response3.body.age).to.eq(bodyUpdate.age);
       });
     });
   });
+});
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,15 +1,18 @@
 import url from '../fixtures/apiPaths.json';
+import dataJoin from '../../data.json'  //the file in .gitignore
 
 // the name of test suit
 describe('Amazon UI Testing', () => {
   let urlAmazon;
+  let emailSigned = dataJoin.email  
+  let passwordSigned = dataJoin.password
 
   //Enter YourEmail and Password First
   beforeEach(() => {
     urlAmazon = `${url.amazonUrl}`;
     cy.visit(urlAmazon);
     cy.get("div[id='nav-link-accountList'] a:nth-child(1)").click();
-    cy.get('#ap_email_login').type('EnteryouEmail');
+    cy.get('#ap_email_login').type(emailSigned);
     cy.get('.a-button-input').click();
 
     Cypress.on('uncaught:exception', (err, runnable) => {
@@ -17,7 +20,7 @@ describe('Amazon UI Testing', () => {
       return false;
     });
 
-    cy.get('#ap_password').type('EnterYourPassword');
+    cy.get('#ap_password').type(passwordSigned);
     cy.get('#signInSubmit').click();
   });
 
@@ -25,23 +28,27 @@ describe('Amazon UI Testing', () => {
   it('tesing amazon products', () => {
     cy.get('.hm-icon').click();
     cy.get('.hmenu-compressed-btn').click();
+
     cy.get(
       '.hmenu-visible > [aria-labelledby="Shop by Category"] > :nth-child(2) > .hidden > :nth-child(11) > .hmenu-item',
     ).click();
+
     cy.get(
       ':nth-child(32) > .category-section > ul > :nth-child(1) > .hmenu-item',
     ).click();
+
     cy.get(
       ':nth-child(2) > .a-unordered-list > .a-spacing-micro > .a-list-item > .a-link-normal > .a-checkbox > label > .a-icon',
     ).click();
-    cy.get('#s-all-filters-announce').click();
+
     cy.xpath("//a[.//span[normalize-space()='New']]").click();
     cy.get('#a-autoid-0-announce').click();
     cy.get('#s-result-sort-select_2').click();
-    cy.get('#s-all-filters-announce').click();
     cy.visit(
       'https://www.amazon.eg/s?i=videogames&rh=n%3A18022560031%2Cp_n_free_shipping_eligible%3A21909080031%2Cp_36%3A-1500000&s=price-desc-rank&dc=&language=en&qid=1744418327&rnid=21909190031&ref=sr_nr_p_36_0_0',
     );
+
+    //For Loop or Use DataForEach
     for (let i = 1; i <= 3; i++) {
       cy.get(`#a-autoid-${i}-announce`).click();
       cy.wait(1000);
